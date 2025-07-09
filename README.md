4af53ac1700cced3542916995eef12075099211c
Alexandra Teran <alexteranr@gmail.com>
2025-07-09 14:27:53 -0600

    Initial clean commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Alexandra Teran Portfolio
---
+++ constants.ts
@@ -0,0 +1,285 @@
+export const METADATA = {
+  title: "Alexandrateran", // Add the title property
+  description: "Welcome to my portfolio website.",
+  siteUrl: "https://www.alexandrateran.com", // Add this property
+};
+
+export const MENULINKS = [
+  {
+    name: "Home",
+    ref: "home",
+  },
+  {
+    name: "Works",
+    ref: "works",
+  },
+  {
+    name: "Skills",
+    ref: "skills",
+  },
+  {
+    name: "Timeline",
+    ref: "timeline",
+  },
+  {
+    name: "Contact",
+    ref: "contact",
+  },
+];
+
+export const TYPED_STRINGS = [
+  "I design and develop things",
+  "I develop modern frontend apps",
+  "I design dynamic user experience",
+  "I design and develop motion",
+];
+
+export const EMAIL = "alexteranr@gmail.com";
+
+export const SOCIAL_LINKS = {
+  linkedin: "https://www.linkedin.com/in/alexandra-teran-503222283",
+  github: "https://www.instagram.com/alexandrateran.r/",
+  instagram: "https://www.instagram.com/alexandrateran.r/",
+}
+
+export interface IProject {
+  name: string;
+  image: string;
+  blurImage: string;
+  description: string;
+  gradient: [string, string];
+  url: string;
+  tech: string[];
+}
+
+export const PROJECTS: IProject[] = [
+  {
+    name: "PROJECT1",
+    image: "/projects/figgen.jpg",
+    blurImage: "/projects/blur/figgen-blur.jpg",
+    description: "Pixel perfect HTML/Tailwind for Figma Auto layout designs.",
+    gradient: ["#1F6582", "#1ABCFE"],
+    url: "https://www.figma.com/community/plugin/1065278044402066626",
+    tech: ["typescript", "figma", "tailwind"],
+  },
+  {
+    name: "ROJECT2",
+    image: "/projects/myokr.jpg",
+    blurImage: "/projects/blur/myokr-blur.jpg",
+    description: "Marketing site for OKR Platform by huminos",
+    gradient: ["#153BB9", "#0E2C8B"],
+    url: "https://www.huminos.com/",
+    tech: ["react", "next", "gsap", "tailwind"],
+  },
+  {
+    name: "PROJECT3",
+    image: "/projects/dlt-website.jpg",
+    blurImage: "/projects/blur/dlt-website-blur.jpg",
+    description: "Marketing site with an Internal CMS from scratch",
+    gradient: ["#245B57", "#004741"],
+    url: "https://www.dltlabs.com/",
+    tech: ["figma", "angular", "gsap"],
+  },
+  {
+    name: "PROJECT4",
+    image: "/projects/dl-unify.jpg",
+    blurImage: "/projects/blur/dl-unify-blur.jpg",
+    description: "Built the application from zero to production 🚀",
+    gradient: ["#003052", "#167187"],
+    url: "https://dlunify.com/",
+    tech: ["tailwind", "angular", "gsap", "figma"],
+  },
+  {
+    name: "PROJECT5",
+    image: "/projects/ngx-quill-upload.jpg",
+    blurImage: "/projects/blur/ngx-quill-upload-blur.jpg",
+    description: "NPM Package for Quill JS uploads from Angular",
+    gradient: ["#3A0000", "#771E1E"],
+    url: "https://www.npmjs.com/package/ngx-quill-upload",
+    tech: ["npm", "angular", "typescript"],
+  },
+  {
+    name: "Huminos website",
+    image: "/projects/huminos.jpg",
+    blurImage: "/projects/blur/huminos-blur.jpg",
+    description: "Marketing site for Huminos bots for workplace by facebook",
+    gradient: ["#17007B", "#3A2C79"],
+    url: "https://bots.huminos.com/",
+    tech: ["javascript", "sass", "svg", "gulp"],
+  },
+  {
+    name: "AKGEC - College Website",
+    image: "/projects/akgec.jpg",
+    blurImage: "/projects/blur/akgec-blur.jpg",
+    description: "Contributed in overall design and development",
+    gradient: ["#5E4C06", "#746528"],
+    url: "https://www.akgec.ac.in/",
+    tech: ["javascript", "html", "css"],
+  },
+  {
+    name: "Alpha Aesthetics",
+    image: "/projects/alpha.jpg",
+    blurImage: "/projects/blur/alpha-blur.jpg",
+    description: "Designed and developed the platform",
+    gradient: ["#172839", "#334659"],
+    url: "https://alpha-aesthetics.ayushsingh.net/",
+    tech: ["illustrator", "javascript", "angular"],
+  },
+  
+];
+
+export const SKILLS = {
+  frontend: [
+    "javascript",
+    "react",
+    "typescript",
+    "vue",
+    "next",
+    "tailwind",
+    "html",
+    "css",
+  ],
+  userInterface: ["figma", "illustrator", "photoshop"],
+
+  other: ["git", "lightroom"],
+  backend: [
+    "springboot",
+    "python",
+    "sql"
+  ],
+};
+
+export enum Branch {
+  LEFT = "leftSide",
+  RIGHT = "rightSide",
+}
+
+export enum NodeTypes {
+  CONVERGE = "converge",
+  DIVERGE = "diverge",
+  CHECKPOINT = "checkpoint",
+}
+
+export enum ItemSize {
+  SMALL = "small",
+  LARGE = "large",
+}
+
+export const TIMELINE: Array<TimelineNodeV2> = [
+  {
+    type: NodeTypes.CHECKPOINT,
+    title: "2025",
+    size: ItemSize.LARGE,
+    shouldDrawLine: false,
+    alignment: Branch.LEFT,
+  },
+  {
+    type: NodeTypes.CHECKPOINT,
+    title: "Full Stack Developer (Current)",
+    size: ItemSize.SMALL,
+    subtitle:
+      "Full Stack Developer @ Compania Licorera (Flor de Cana)",
+    image: "/timeline/logoflordecana.svg",
+    slideImage: "/timeline/flordecana.jpg",
+    shouldDrawLine: true,
+    alignment: Branch.LEFT,
+  },
+  {
+    type: NodeTypes.CHECKPOINT,
+    title: "Graduated from College 🎓",
+    size: ItemSize.SMALL,
+    subtitle:
+      "Spent 4 years laying the foundation of Frontend Engineering, UI/UX, and Information Technology!",
+      image: "/timeline/tculogo.svg",
+      slideImage: "/timeline/finalpresentationme.jpg",
+    shouldDrawLine: true,
+    alignment: Branch.LEFT,
+  },
+  {
+    type: NodeTypes.CHECKPOINT,
+    title: "Data Analytics Intern",
+    size: ItemSize.SMALL,
+    subtitle:
+      "Data Analytics @ Generadora Electrica Occidental (GEOSA)",
+    slideImage: "/timeline/GEOSA.jpg",
+    shouldDrawLine: true,
+    alignment: Branch.LEFT,
+  },
+  {
+    type: NodeTypes.CHECKPOINT,
+    title: "2024",
+    size: ItemSize.LARGE,
+    shouldDrawLine: false,
+    alignment: Branch.LEFT,
+  },
+  {
+    type: NodeTypes.CHECKPOINT,
+    title: "Copy Editor",
+    size: ItemSize.SMALL,
+    subtitle:
+      "Edit and analyzed all news reports to the TCU360 Website @ TCU360",
+      image: "/timeline/tculogo.svg",
+    slideImage: "/timeline/logotcu.jpg",
+    shouldDrawLine: true,
+    alignment: Branch.LEFT,
+  },
+  {
+    type: NodeTypes.CHECKPOINT,
+    title: "2023",
+    size: ItemSize.LARGE,
+    shouldDrawLine: false,
+    alignment: Branch.LEFT,
+  },
+  {
+    type: NodeTypes.CHECKPOINT,
+    title: "IT & Data Analyst",
+    size: ItemSize.SMALL,
+    subtitle:
+      "Joined a competitive IT Internship program within a leading Nicaraguan bank as an IT & Data Analyst @ Banco de Finanzas (BDF) ",
+      image: "/timeline/bdflogo.svg",
+      slideImage: "/timeline/bdf.jpg",
+    shouldDrawLine: true,
+    alignment: Branch.LEFT,
+  },
+ 
+
+  {
+    type: NodeTypes.CHECKPOINT,
+    title: "2022",
+    size: ItemSize.LARGE,
+    shouldDrawLine: false,
+    alignment: Branch.LEFT,
+  },
+  {
+    type: NodeTypes.CHECKPOINT,
+    title: "IT Help Desk",
+    size: ItemSize.SMALL,
+    subtitle:
+      "Delivereed IT support to over +5,000 employees, resolving technical issues and ensuring smooth operations @TCU Library Computer Lab.",
+      image: "/timeline/tculogo.svg",
+      slideImage: "/timeline/tcu.jpg",
+    shouldDrawLine: true,
+    alignment: Branch.LEFT,
+  },
+  
+  
+];
+
+export type TimelineNodeV2 = CheckpointNode | BranchNode;
+
+export interface CheckpointNode {
+  type: NodeTypes.CHECKPOINT;
+  title: string;
+  subtitle?: string;
+  size: ItemSize;
+  image?: string;
+  slideImage?: string;
+  shouldDrawLine: boolean;
+  alignment: Branch;
+}
+
+export interface BranchNode {
+  type: NodeTypes.CONVERGE | NodeTypes.DIVERGE;
+}
+
+export const GTAG = "UA-163844688-1";
